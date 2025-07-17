d0016b47b8346813c1b4eb94a5872a711cf73987
Kubo2025-lab <eduardo@kuboasesorias.cl>
2025-07-17 18:10:17 -0400

    Add files via upload

---
+++ src/components/sections/About.js
@@ -0,0 +1,195 @@
+const About = () => {
+  return (
+    <section className="about main-section flex-column-mobile" id="about">
+      {/* INFO HOLDER STARTS */}
+      <div className="info flex-column-mobile">
+        {/* IMAGE STARTS */}
+        <div
+          className="img-container animated-layer image-animation my-photo-container fadeInUp wow"
+          data-wow-offset={200}
+          id="my-photo"
+        >
+          <div>
+            <div>
+              <img className="my-photo" src="assets/about.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+        {/* IMAGE ENDS */}
+        {/* INFO STARTS */}
+        <div>
+          <h2>
+            <span>
+              <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                Kubo
+              </span>
+            </span>
+            <span>
+              <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                Servicios
+              </span>
+            </span>
+          </h2>
+          <div className="infos">
+            <ul>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Experiencia :</span>
+                    <span>12 años</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Nacionalidad :</span>
+                    <span>Chilena</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Empresa :</span>
+                    <span>Formalizada</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Lenguaje :</span>
+                    <span>Español</span>
+                  </span>
+                </span>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Dirección :</span>
+                    <span>Merced N° 838 of. 117, Santiago</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Fono :</span>
+                    <span>+569 2179 2366</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Email :</span>
+                    <span>eduardo@kuboasesorias.cl</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Linkedin :</span>
+                    <span>/in/eduardo-zambrano/</span>
+                  </span>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* INFO ENDS */}
+      </div>
+      {/* INFO HOLDER ENDS */}
+  
+      {/* SKILLS STARTS */}
+      <div className="skills flex-column-mobile">
+        <div className="custom-title">
+          <h3>
+            <span>
+              <span className="animated-layer fade-in-left-animation fadeInUp wow">
+                #Servicios
+              </span>
+            </span>
+          </h3>
+        </div>
+
+        <div className="skills-content">
+          {/* Bloque 1 */}
+          <div>
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="fas fa-chart-line" />
+              </span>
+              <h4>Gestión Empresarial</h4>
+            </div>
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="fas fa-coins" />
+              </span>
+              <h4>Ejecución Presupuestaria</h4>
+            </div>
+          </div>
+
+          {/* Bloque 2 */}
+          <div>
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="fas fa-bullseye" />
+              </span>
+              <h4>Estrategias Comerciales</h4>
+            </div>
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="fas fa-check-circle" />
+              </span>
+              <h4>Metodología SMART</h4>
+            </div>
+          </div>
+
+          {/* Bloque 3 */}
+          <div>
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="fas fa-briefcase" />
+              </span>
+              <h4>Venta a Gobierno (B2G)</h4>
+            </div>
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="fas fa-file-signature" />
+              </span>
+              <h4>Estudio de Propuestas</h4>
+            </div>
+          </div>
+
+          {/* Bloque 4 */}
+          <div>
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="fas fa-project-diagram" />
+              </span>
+              <h4>Planificación de Proyectos</h4>
+            </div>
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="fas fa-funnel-dollar" />
+              </span>
+              <h4>Flujos Financieros</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* SKILLS ENDS */}
+
+      {/* Puedes continuar con tu sección de RESUME aquí si la vas a mantener */}
+
+      <img alt="" className="separator hide-mobile" src="assets/separator.png" />
+    </section>
+  );
+};
+
+export default About;
