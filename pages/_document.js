69f82bb3b1733c9743779c6bb6f7eae8089322ea
Kubo2025-lab <eduardo@kuboasesorias.cl>
2025-07-18 17:51:44 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es">
       <Head />
       <body>
+        {/* GTM noscript fallback para navegadores sin JS */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NX74D4K"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
+
