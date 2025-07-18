18fb38ef8a2a07cb542ec422bafc0983ee95cf1f
Kubo2025-lab <eduardo@kuboasesorias.cl>
2025-07-17 20:35:36 -0400

    Update About.js

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -185,11 +185,106 @@ const About = () => {
       </div>
       {/* SKILLS ENDS */}
 
-      {/* Puedes continuar con tu sección de RESUME aquí si la vas a mantener */}
-
-      <img alt="" className="separator hide-mobile" src="assets/separator.png" />
+      {/* TIMELINE STARTS */}
+        <div className="timeline">
+          <ol className="animated-layer fade-in-animation">
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-down-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Project Manager</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>2019 - Now</span>
+                  </p>
+                  <p>
+                    <i className="fa-regular fa-building" />
+                    <span>Adobe Company</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-up-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Team Leader</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>2015 - 2019</span>
+                  </p>
+                  <p>
+                    <i className="fa-regular fa-building" />
+                    <span>Twitter Inc</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-down-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Web Developer</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>2010 - 2015</span>
+                  </p>
+                  <p>
+                    <i className="fa-regular fa-building" />
+                    <span>Facebook</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-up-animation fadeInUp wow">
+                <div className="education">
+                  <h4>Master Degree</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>2003</span>
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-building-columns" />
+                    <span>London University</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-down-animation fadeInUp wow">
+                <div className="education">
+                  <h4>Bachelor Degree</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>1999</span>
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-building-columns" />
+                    <span>Berlin High School</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            <li />
+          </ol>
+        </div>
+        {/* TIMELINE ENDS */}
+      </div>
+      {/* RESUME ENDS */}
+      <img
+        alt=""
+        className="separator hide-mobile"
+        src="assets/separator.png"
+      />
     </section>
   );
 };
-
 export default About;
