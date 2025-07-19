61e2237516017e59f0e60a5ffa90e1f33b7c79f5
Kubo2025-lab <eduardo@kuboasesorias.cl>
2025-07-18 21:45:24 -0400

    Update post-adjudicacion.js

--- pages/servicios/post-adjudicacion.js
+++ pages/servicios/post-adjudicacion.js
@@ -1,9 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
-import Contact from "../../components/sections/Contact";
-import Blog from "../../components/sections/Blog";
-import Portfolio from "../../components/sections/Portfolio";
-import "../../styles/style.css";
+
+
 export default function PostAdjudicacion() {
   const [formEnviado, setFormEnviado] = useState(false);
 
