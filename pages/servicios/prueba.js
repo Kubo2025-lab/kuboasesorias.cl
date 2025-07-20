6ba6a20e95134d75061e289cdb1b36dede78cca6
Kubo2025-lab <eduardo@kuboasesorias.cl>
2025-07-20 10:18:29 -0400

    Update prueba.js

--- pages/servicios/prueba.js
+++ pages/servicios/prueba.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { Fragment } from "react";
 import Header from "../../src/components/layouts/Header";
-import Footer from "../../src/components/layouts/Footer";
 import Home from "../../src/components/sections/Home";
 import Portfolio from "../../src/components/sections/Portfolio";
 import Testimonials from "../../src/components/sections/Testimonials";
