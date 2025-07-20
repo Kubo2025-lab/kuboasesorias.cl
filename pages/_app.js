e6527c1407988ef16373112e1188029d86983518
Kubo2025-lab <eduardo@kuboasesorias.cl>
2025-07-20 10:59:47 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Preloader from "@/src/components/Preloader";
+import Switcher from "@/src/components/Switcher";
 import SalimovHead from "@/src/SalimovHead";
 import "@/styles/globals.css";
 import { Fragment } from "react";
@@ -15,23 +16,25 @@ const App = ({ Component, pageProps }) => {
         id="gtm-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
+          __html: 
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-NX74D4K');
-          `,
+          ,
         }}
       />
 
       {/* Estilos y preloader */}
-      <Preloader />
       <Switcher />
+      <Preloader />
+
       {/* Render de la página */}
       <Component {...pageProps} />
     </Fragment>
   );
 };
 
+export default App;
 export default App;
